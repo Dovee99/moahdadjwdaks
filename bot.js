a298f81a7b973ac68818655321dfb90d0b50d56c
Dovee99 <44682400+Dovee99@users.noreply.github.com>
2019-02-25 18:12:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("-new",{type: 'WATCHING'})
+
+ 
 
  client.on("message", (message) => {
 
